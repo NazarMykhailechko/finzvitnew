b343a138261889ae06d7c7a6a9ef5baa2da7cfda
unknown <WIN72007@ukr.net>
2023-11-22 00:29:33 +0200

    add founders and beneficiares

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "shinythemes", "jsonlite", "reshape2", "dplyr", "tidyr", "openxlsx", "shinyauthr", "shinyjs", "data.table", "RMySQL")
+my_packages = c("shiny", "shinythemes", "jsonlite", "reshape2", "dplyr", "tidyr", "openxlsx", "shinyauthr", "shinyjs", "data.table", "RMySQL","rvest","httr")
 
 
 install_if_missing = function(p) {
