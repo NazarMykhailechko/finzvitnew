04e625a587495a354efcc080acba2d4d8bfcd082
unknown <win72007@ukr.net>
2025-06-05 15:13:03 +0300

    forst commit

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("randomForest", "data.table")
+my_packages = c("shiny", "data.table","shinyBS","shinythemes","jsonlite", "reshape2", "dplyr", "tidyr", "openxlsx", "shinyauthr","shinyjs", "RMySQL", "XML", "xml2", "shinyWidgets")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
